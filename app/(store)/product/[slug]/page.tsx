fbf8dc3ea787a6caff56f247b759e7ba114e8279
heistifeh <teefeh26@gmail.com>
2025-06-27 20:44:07 +0100

    simple correction and title

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -50,7 +50,7 @@ async function ProductPage(props: { params: Params }) {
           <div>
             <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
             <div className="text-xl font-semibold mb-4">
-              ₦{product.price?.toFixed(2)}
+              ${product.price?.toFixed(2)}
             </div>
             <div className="prose max-w-none mb-6">
               {Array.isArray(product.description) && (
